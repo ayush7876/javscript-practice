f347e1db563b805733f84848b98b1b37f218fcf5
ayush7876 <aaushmaan1988@gmail.com>
2024-07-24 16:03:44 +0000

    commit

---
+++ .devcontainer/01_basics/test.js
@@ -0,0 +1,15 @@
+// console.log("ayush")
+// console.log("ayush");
+const accountId = 144553  
+let accountEmail ="hitesh@google.com"
+var accountpassword ="12345"
+// accountCity = "jaipur"
+let accountState;
+
+//acccountid= 2 //not allowed
+
+ accountEmail="hc@hc.com"
+ accountpassword="21212121"
+// accountcity="bengaluru"
+
+console.table([accountId,accountEmail,accountpassword,accountState]) 
---
+++ .devcontainer/datatypes.js
@@ -0,0 +1,40 @@
+"use strict ";//treat all js code ss newer version 
+//alert (3+3) //we are using nide js not browser
+
+
+console.log (3+3)
+//code readability should be hgh 
+console.log("ayush")
+// condole
+
+//mdn site pe or tc39.s sab milega js ka 
+ 
+//datatypes 
+let name = "hitesh "//string
+let age = 18 //number
+let isloggedIn = false // boolean 
+
+//number =>2 to power 53
+//bigint 
+//string=>" "
+//boolean => true or false 
+//null => stand alone value 
+//undefined =>
+//symbol to find the uniqueness among the symbols
+
+//object
+
+console.log(typeof undefined );//type undefine aayega
+console.log(typeof null); //object
+console.log ("ayush");
+
+
+let score ="33abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log (typeof valueInNumber);
+console.log(valueInNumber);
+// console.log("ayush")
