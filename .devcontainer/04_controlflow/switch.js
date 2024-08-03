229cee9a854dee2836c3f17c1d8f12fcbc71ea9d
ayush7876 <aaushmaan1988@gmail.com>
2024-08-03 02:10:48 +0000

    commit

---
+++ .devcontainer/04_controlflow/switch.js
@@ -0,0 +1,31 @@
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+// yaha pe jo break statement use hua hai ye isiliye hua hai taaki hum is flow ka break tod sake agar break nahib hota to jaha match hojaata uske aage vala code bhi execute hojata hai 
